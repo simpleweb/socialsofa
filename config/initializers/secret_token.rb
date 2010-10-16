54ac66457f8b9c5aa29e405bd388d71595586676
Chris Mytton <self@hecticjeff.net>
2010-10-16 02:00:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialsofa::Application.config.secret_token = 'f25275038771ed0468755bd3d089f3e421a8ba35a2d1a1ad6dcbc56a5abce358e5656ee823c421bebe25120bc56b6c83431570138307ffc9b86001ecbcd553a5'

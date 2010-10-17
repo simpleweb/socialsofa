584e9c4c10bbe21bef9716a3cc9b135460a95f90
Chris Mytton <self@hecticjeff.net>
2010-10-17 17:31:22 +0100

    Tests

--- test/unit/subscription_test.rb
+++ test/unit/subscription_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class SubscriptionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should validate_uniqueness_of(:topic)
+  should validate_presence_of(:topic)
+
+  should allow_value('http://search.twitter.com/search.atom?q=valid').for(:topic)
+  should_not allow_value('not valid').for(:topic)
+  should_not allow_value('invalid').for(:topic)
 end

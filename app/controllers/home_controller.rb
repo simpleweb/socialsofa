a9c053ecb9f2be9f1a24828d63776297e7fe3445
Chris Mytton <self@hecticjeff.net>
2010-10-17 21:41:40 +0100

    Couch views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
+require 'simple_couch'
+require 'pp'
+
 class HomeController < ApplicationController
   def index
   end
   def search
+    @query = params[:q]
+    if @query
+      matches = SimpleCouch.search_by_content(params[:q]).parsed_response
+      @results = []
+      matches["rows"].each do |result|
+        @results << SimpleCouch.get('/'+result["id"]).parsed_response
+      end
+    end
   end
+
 end

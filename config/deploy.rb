c8251b5cde16185b8e608056777df22353e07083
Chris Mytton <self@hecticjeff.net>
2010-10-17 22:05:38 +0100

    Settings for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,28 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "socialsofa"
+set :repository,  "git@github.com:railsrumble/rr10-team-262.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :scm_username, "hecticjeff"
+set :branch, :master
+set :deploy_via, :remote_cache
+
+set :deploy_to, "/var/www"
+set :use_sudo, true
+
+role :app, "socialsofa.r10.railsrumble.com"
+role :web, "socialsofa.r10.railsrumble.com"
+role :db, "socialsofa.r10.railsrumble.com"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
